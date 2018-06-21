502e35a2b7e08debab41fe6ea06d90467074c3ae
balresch <balresch@gmail.com>
2018-06-21 14:06:52 +0200

    Initial commit

---
+++ words.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""Retrieve and print words from a URL.
+
+Usage:
+    python words.py <URL>
+"""
+
+import sys
+from urllib.request import urlopen
+
+def fetch_words(url):
+    """Fetch a list of words from a URL.
+
+    Args:
+        url: The URL of a UTF-8 text document.
+
+        Returns:
+            A list of strings containing the words from the document.
+    """
+    with urlopen(url) as story:
+        story_words = []
+        for line in story:
+            line_words = line.decode("utf-8").split()
+            for word in line_words:
+                story_words.append(word)
+    return story_words
+
+
+def print_items(items):
+    """Print items one per line.
+
+    Args:
+        An iterable series of printable items.
+    """
+    for item in items:
+        print(item)
+
+
+def main(url):
+    """Print each word fropm a text document from a URL.
+
+    Args:
+        url: The URL of a UTF-8 text document.
+    """
+    words = fetch_words(url)
+    print_items(words)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])   # the first argument [0] would be the module filename itself, so [1} is the first additional arg
